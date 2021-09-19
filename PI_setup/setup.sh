1c3e7ebc848dc791630bb66cc6ac4a486155341a
David Gonzalez Leon <david.gonzalez@citycable.ch>
2021-09-19 15:41:51 +0200

    minor fix script

--- PI_setup/setup.sh
+++ PI_setup/setup.sh
@@ -1,3 +1,5 @@
+sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get install automake libtool vim cmake libusb-1.0-0-dev libx11-dev xorg-dev libglu1-mesa-dev
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 source ~/.bashrc
 cd ~
