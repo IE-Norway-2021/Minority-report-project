ce62140739634e19071e47e07e20406b50ebd5f8
David Gonzalez Leon <david.gonzalez@citycable.ch>
2021-09-30 10:03:52 +0200

    main update

--- cnn_test/main.py
+++ cnn_test/main.py
@@ -9,8 +9,47 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPool2D
+from keras.utils import np_utils
+
+
+def cnn():
+    # Flattening the images from the 28x28 pixels to 1D 787 pixels
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    X_train = X_train.reshape(60000, 784)
+    X_test = X_test.reshape(10000, 784)
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+
+    # normalizing the data to help with the training
+    X_train /= 255
+    X_test /= 255
+
+    # one-hot encoding using keras' numpy-related utilities
+    n_classes = 10
+    print("Shape before one-hot encoding: ", y_train.shape)
+    Y_train = np_utils.to_categorical(y_train, n_classes)
+    Y_test = np_utils.to_categorical(y_test, n_classes)
+    print("Shape after one-hot encoding: ", Y_train.shape)
+
+    # building a linear stack of layers with the sequential model
+    model = Sequential()
+    # hidden layer
+    model.add(Dense(100, input_shape=(784,), activation='relu'))
+    # output layer
+    model.add(Dense(10, activation='softmax'))
+
+    # looking at the model summary
+    model.summary()
+    # compiling the sequential model
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+    # training the model for 10 epochs
+    model.fit(X_train, Y_train, batch_size=128, epochs=10, validation_data=(X_test, Y_test))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cnn()
