3c5d8fb8cc7f76005e865c54b97946237cf519d8
David Gonzalez Leon <david.gonzalez@citycable.ch>
2021-10-18 10:34:59 +0200

    added depth training function

--- CNN/ML_code/main.py
+++ CNN/ML_code/main.py
@@ -17,9 +17,9 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 img_height = 120
 img_width = 160
 batch_size = 12
-folder_name = 'rgb_image_dataset'
+folder_name = 'video_test_dataset'
 split_value = 0.1
-EPOCHS = 20
+EPOCHS = 30
 INIT_LR = 0.00001
 actions = np.array(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
 PERCENT = 25
@@ -121,7 +121,7 @@ def test():
     model.save('rgb_landmark_weights.h5')
 
 
-def test2():
+def rgb_new():
     print('Starting Image loading...')
     label_map = {label: num for num, label in enumerate(actions)}
     images, labels = [], []
@@ -143,9 +143,10 @@ def test2():
                           activation='relu'),
             layers.MaxPooling2D(),
             layers.Conv2D(64, 3, padding="same", activation="relu"),
+            layers.Conv2D(32, 3, padding="same", activation="relu"),
             layers.MaxPooling2D(),
             layers.BatchNormalization(),
-            layers.Conv2D(64, 3, padding="same", activation="relu"),
+            layers.Conv2D(32, 3, padding="same", activation="relu"),
             layers.MaxPooling2D(),
             layers.BatchNormalization(),
             layers.Flatten(),
@@ -186,6 +187,95 @@ def test2():
     model2.save('rgb_only_new_weights.h5')
 
 
+def depth_new():
+    label_map = {label: num for num, label in enumerate(actions)}
+    images, labels = [], []
+    for action in actions:
+        for dirpath, dirnames, files in os.walk(os.path.join(folder_name, action)):
+            action_set = []
+            for file_name in files:
+                img = cv2.imread(os.path.join(folder_name, action, file_name))
+                if np.array(img).shape != (480, 640, 3):
+                    img = cv2.resize(img, (640, 480), interpolation=cv2.INTER_AREA)
+                if np.array(img).shape != (480, 640, 3):
+                    print('Error')
+                images.append(resize_image(img))
+                labels.append(label_map[action])
+    X = np.array(images)
+    y = to_categorical(labels).astype(int)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=split_value)
+    model3 = keras.Sequential(
+        [
+            layers.Conv2D(128, kernel_size=(3, 4), input_shape=(120, 160, 3), strides=(1, 1), padding='valid',
+                          activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, 3, padding="same", activation="relu"),
+            layers.Conv2D(32, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.BatchNormalization(),
+            layers.Conv2D(32, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.BatchNormalization(),
+            layers.Flatten(),
+            layers.Dense(200, activation='relu'),
+            layers.Dense(100, activation='relu'),
+            layers.Dropout(0.4),
+            layers.Dense(10, activation='softmax'),
+        ]
+    )
+
+    model3.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS),
+        loss='categorical_crossentropy', metrics=["accuracy"],
+    )
+    model3.summary()
+    history = model3.fit(X_train, y_train, epochs=EPOCHS, verbose=1, validation_data=(X_val, y_val))
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(EPOCHS)
+
+    plt.figure(figsize=(15, 15))
+    plt.subplot(2, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+    model3.save('depth_new_weights.h5')
+
+
+movements = np.array(['scroll_right', 'scroll_left', 'scroll_up', 'scroll_down', 'zoom_in', 'zoom_out'])
+
+
+def video_rgb_ml():
+    root = 'test/rgb'
+    label_map = {label: num for num, label in enumerate(movements)}
+    sequences, labels = [], []
+    for movement in movements:
+        for dirpath, dirnames, files in os.walk(os.path.join(root, movement)):
+            sequence = []
+            for file_name in files:
+                img = cv2.imread(os.path.join(dirpath, file_name))
+                sequence.append(resize_image(img))
+                labels.append(label_map[movement])
+            if len(sequence) > 0:
+                sequences.append(sequence)
+                labels.append(label_map[movement])
+    X = np.array(sequences)
+    y = to_categorical(labels).astype(int)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=split_value)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    test2()
+    video_rgb_ml()
