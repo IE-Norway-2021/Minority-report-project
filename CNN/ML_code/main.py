c4fabe9f670a34b2af7be8624a515be617a08e84
David Gonzalez Leon <david.gonzalez@citycable.ch>
2021-11-02 15:03:39 +0100

    removed logger from ml

--- CNN/ML_code/main.py
+++ CNN/ML_code/main.py
@@ -380,26 +380,8 @@ def video_depth_reduced_ml():
     video_ml('video_dataset/depth', 'video_depth_reduced', reduced=True)
 
 
-class Logger(object):
-    def __init__(self):
-        self.terminal = sys.stdout
-        self.log = open("output.txt", "a")
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-
-    def flush(self):
-        # this flush method is needed for python 3 compatibility.
-        # this handles the flush command by doing nothing.
-        # you might want to specify some extra behavior here.
-        pass
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # Redirect output
-    sys.stdout = Logger()
     print('Doing rgb training...')
     video_rgb_ml()
     print('Doing depth training...')
