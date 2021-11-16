57362d8020c96a3843ac74a749ba6278df207e4d
David Gonzalez Leon <david.gonzalez@citycable.ch>
2021-11-16 14:41:25 +0100

    add requ python to setup

--- PI_setup/setup.sh
+++ PI_setup/setup.sh
@@ -37,7 +37,7 @@ export PYTHONPATH=$PYTHONPATH:/usr/local/lib
 export PYTHONPATH=$PYTHONPATH:/usr/lib/python3/dist-packages/pyrealsense2
 source ~/.bashrc
 
-pip3 install opencv-python matplotlib numpy sklearn pillow pyrealsense2 
+pip3 install req.txt 
 
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
