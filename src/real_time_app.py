c3f2baf94642b16ab3fc0292008cc571daeb092f
jade <jade.groli@heig-vd.ch>
2021-11-16 14:43:42 +0100

    added tensorflow lite + convert h5 to tflite

--- src/real_time_app.py
+++ src/real_time_app.py
@@ -1,1 +1,148 @@
-import tf.lite as tf
+# import tf.lite as tf
+# import tflite_runtime as tf
+import tensorflow as tf
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+img_height = 120
+img_width = 160
+sequence_length = 10
+PERCENT = 25
+rate = 30
+
+actions = ['scroll_right', 'scroll_left', 'scroll_up', 'scroll_down', 'zoom_in', 'zoom_out']
+
+
+def resize_image(img):
+    width = int(img.shape[1] * PERCENT / 100)
+    height = int(img.shape[0] * PERCENT / 100)
+    dim = (width, height)
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    return resized
+
+
+def main_app_reduced_2_full():
+
+    # Load TFLite model and allocate tensors.
+    interpreter_rgb = tf.lite.Interpreter(model_path="video_rgb_reduced_2_weights.tflite")
+    interpreter_depth = tf.lite.Interpreter(model_path="video_depth_reduced_2_weights.tflite")
+
+    # Get input and output tensors.
+    input_details_rgb = interpreter_rgb.get_input_details()
+    output_details_rgb = interpreter_rgb.get_output_details()
+    input_details_depth = interpreter_depth.get_input_details()
+    output_details_depth = interpreter_depth.get_output_details()
+    
+    #model_rgb.load_weights('video_rgb_reduced_2_weights.h5')
+    #model_depth.load_weights('video_depth_reduced_2_weights.h5')
+    print('Finished loading models')
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+
+    # Get device product line for setting a supporting resolution
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+    found_rgb = False
+    for s in device.sensors:
+        if s.get_info(rs.camera_info.name) == 'RGB Camera':
+            found_rgb = True
+            break
+    if not found_rgb:
+        print("The demo requires Depth camera with Color sensor")
+        exit(0)
+
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, rate)
+
+    if device_product_line == 'L500':
+        config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, rate)
+    else:
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, rate)
+
+    colorizer = rs.colorizer()
+    colorizer.set_option(rs.option.color_scheme, 0)
+
+    print('Starting streaming...')
+
+    # Start streaming
+    pipeline.start(config)
+
+    frame_counter = 0
+    sequence_rgb = []
+    sequence_depth = []
+    threshold = 0.7
+    nb_of_frames = 20
+    last_preds = []
+    validation_num = 6
+
+    try:
+        while True:
+            try:
+                frames = pipeline.wait_for_frames()
+            except RuntimeError:
+                continue
+            frame_counter = (frame_counter + 1) % 2
+            if frame_counter != 0:
+                continue
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
+
+            # Convert images to numpy arrays
+            color_image = np.asanyarray(color_frame.get_data())
+
+            color_image = resize_image(color_image)
+            depth_image = resize_image(
+                cv2.resize(np.asanyarray(colorizer.colorize(depth_frame).get_data()), (640, 480),
+                           interpolation=cv2.INTER_AREA))
+            sequence_rgb.append(color_image)
+            sequence_depth.append(depth_image)
+
+            if len(sequence_rgb) >= nb_of_frames:
+                sequence_depth = sequence_depth[-nb_of_frames:]
+                sequence_rgb = sequence_rgb[-nb_of_frames:]
+                # pred_rgb = model_rgb.predict(np.expand_dims(sequence_rgb, axis=0))[0]
+                # pred_depth = model_depth.predict(np.expand_dims(sequence_depth, axis=0))[0]
+
+                interpreter_rgb.set_tensor(input_details_rgb[0]['index'], [sequence_rgb])
+                interpreter_depth.set_tensor(input_details_depth[0]['index'], [sequence_depth])
+                interpreter_rgb.invoke()
+                interpreter_depth.invoke()
+
+                pred_rgb = interpreter_rgb.get_tensor(output_details_rgb[0]['index'])
+                pref_depth = interpreter_depth.get_tensor(output_details_depth[0]['index'])
+
+                print("the rgb output is {}".format(pred_rgb))
+                print("the depth output is {}".format(pred_depth))
+
+                all_valid = True
+                for test_res in [pred_rgb, pred_depth]:
+                    if test_res[np.argmax(test_res)] < threshold:
+                        all_valid = False
+                        break
+                if not all_valid:
+                    continue
+                if np.argmax(pred_rgb) == np.argmax(pred_depth):
+                    last_preds.append(np.argmax(pred_depth))
+                    if len(last_preds) >= validation_num:
+                        result = all(elem == last_preds[0] for elem in last_preds)
+                        if result:
+                            print(f'Pred values : rgb={pred_rgb[np.argmax(pred_rgb)]}, depth={pred_depth[np.argmax(pred_depth)]}')
+                            last_preds.clear()
+                            sequence_rgb = sequence_rgb[-1:]
+                            sequence_depth = sequence_depth[-1:]
+                            print(f'all agreed it was a {actions[np.argmax(pred_rgb)]}')
+                        else:
+                            last_preds = last_preds[-(validation_num-1):]
+
+    finally:
+        pipeline.stop()
+
+
+if __name__ == '__main__':
+    main_app_reduced_2_full()
