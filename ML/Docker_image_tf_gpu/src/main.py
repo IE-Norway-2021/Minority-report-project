7d5d77044b630ccc458b1c0e5c5eb41e8dc7735e
David Gonzalez Leon <david.gonzalez@citycable.ch>
2021-11-16 14:39:22 +0100

    dockerfile for ml

---
+++ ML/Docker_image_tf_gpu/src/main.py
@@ -0,0 +1,5 @@
+#This python file is used to make sure the docker stays online even after you exit
+
+print("Waiting forever...")
+while True:
+   continue
---
+++ ML/Docker_image_tf_gpu/start_container.sh
@@ -0,0 +1,1 @@
+docker run --gpus all -it ml/gesture /bin/bash
