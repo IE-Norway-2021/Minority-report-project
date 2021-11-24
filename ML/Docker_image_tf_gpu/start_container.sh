a1213e2b33ec701c5c05e440549af3a096a46a09
David Gonzalez Leon <david.gonzalez@citycable.ch>
2021-11-24 09:35:40 +0100

    added more scripts for docker container

--- ML/Docker_image_tf_gpu/start_container.sh
+++ ML/Docker_image_tf_gpu/start_container.sh
@@ -1,2 +1,2 @@
-docker run --gpus all -t -d --name gesture_ml_container ml/gesture 
+docker run --gpus all -t -d --shm-size=1g --ulimit memlock=-1 --name gesture_ml_container ml/gesture
 docker exec -it gesture_ml_container /bin/bash
