6f4518f4122b245c5cef25d209c23277ed04975e
jade <jade.groli@heig-vd.ch>
2021-11-25 09:40:36 +0100

    rewrok setup scripts, atomic lib error

--- PI_setup/setup.sh
+++ PI_setup/setup.sh
@@ -1,49 +1,13 @@
 sudo apt-get update && sudo apt-get dist-upgrade
-sudo apt-get install -y automake libtool cmake libusb-1.0-0-dev libx11-dev xorg-dev libglu1-mesa-dev libssl-dev python3 python3-dev pip raspberrypi-kernel-headers swig3.0
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-source ~/.bashrc
-# install python3.7 (version for mediapipe and tensorflow)
+sudo apt-get install automake libtool nano cmake libusb-1.0-0-dev libx11-dev xorg-dev libglu1-mesa-dev libssl-dev 
+
+sudo cp dphys-swapfile /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile restart swapon -s
 cd ~
-git clone --depth=1 -b v3.10.0 https://github.com/google/protobuf.git
-cd protobuf
-./autogen.sh
-./configure
-make -j1
-sudo make install
-cd python
-export LD_LIBRARY_PATH=../src/.libs
-python3 setup.py build --cpp_implementation 
-python3 setup.py test --cpp_implementation
-sudo python3 setup.py install --cpp_implementation
-export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
-export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=3
-sudo ldconfig
-protoc --version
-cd ~
-wget https://github.com/PINTO0309/TBBonARMv7/raw/master/libtbb-dev_2018U2_armhf.deb
-sudo dpkg -i ~/libtbb-dev_2018U2_armhf.deb
-sudo ldconfig
-rm libtbb-dev_2018U2_armhf.deb
-cd ~/librealsense
-mkdir  build  && cd build
-cmake .. -DBUILD_EXAMPLES=true -DCMAKE_BUILD_TYPE=Release -DFORCE_LIBUVC=true
-make -j1
-sudo make install
-cd ~/librealsense/build
-cmake .. -DBUILD_PYTHON_BINDINGS=bool:true -DPYTHON_EXECUTABLE=$(which python3)
-make -j1
-sudo make install
-export PYTHONPATH=$PYTHONPATH:/usr/local/lib
-export PYTHONPATH=$PYTHONPATH:/usr/lib/python3/dist-packages/pyrealsense2
-source ~/.bashrc
+git clone https://github.com/IntelRealSense/librealsense.git
+cd librealsense
+sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/ 
 
-pip3 install tensorflow opencv-python matplotlib numpy sklearn pillow pyrealsense2 
-
-sudo addgroup uinput
-sudo adduser pi uinput
-sudo cp 99-uinput.rules /etc/udev/rules.d/99-uinput.rules
-
-sudo apt-get install -y python-opengl
-sudo -H pip3 install pyopengl
-sudo -H pip3 install pyopengl_accelerate==3.1.3rc1
-echo 'Now please enable the OpenGL ine the advanced options (sudo raspi-config) and reboot the computer'
+echo "enter the following command as su : udevadm control --reload-rules && udevadm trigger"
+echo "Then type : \"exit\" and run ./setup_2.sh"
+sudo su
